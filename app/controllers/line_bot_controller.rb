03f790befe457d4abcd7b3ade67279c53af0b40b
miyaichi <burnedsoba@gmail.com>
2023-03-15 12:03:09 +0900

    modify 入力の書式を変更

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -17,17 +17,16 @@ class LineBotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           strings = event.message['text'].split(/\R/)
-          return head :ok unless strings[0] == '@家計管理' || strings[0] == '＠家計管理'
 
-          case strings[1]
+          case strings[0]
           when '登録'
-            text = regist_cost(strings[2], strings[3].to_i, event['source']['userId'])
+            text = regist_cost(strings[1], strings[2].to_i, event['source']['userId'])
           when '確認'
             text = list_expenses(event['source']['userId'])
           when '精算'
-            text = pay_expenses(strings[2])
+            text = pay_expenses(strings[1])
           else
-            text = "😄使い方😄\n以下の書式に従ってメッセージを送信してください。\n\n✏️費用の登録✏️\n＠家計管理\n登録\n費用の名前\n金額\n\n🔍費用の確認🔍\n＠家計管理\n確認\n\n💰費用の精算💰\n＠家計管理\n精算\n精算したいID（,区切りで複数可能）\n\n使い方を再度見るには、「＠家計管理」だけで送信してください。"
+            text = "😄使い方😄\n以下の書式に従ってメッセージを送信してください。\n\n✏️費用の登録✏️\n登録\n費用の名前\n金額\n\n🔍費用の確認🔍\n確認\n\n💰費用の精算💰\n精算\n精算したいID（,区切りで複数可能）\n\n使い方を再度見るには、「ヘルプ」と送信してください。"
           end
 
           message = {
@@ -68,28 +67,31 @@ class LineBotController < ApplicationController
 
     text = "未精算の一覧を出力します。\n"
     expenses.each do |expense|
-      text << "#{expense.id} #{expense.name} #{expense.cost.to_formatted_s(:delimited)}円\n"
+      text << "#{expense.id}|#{expense.name}|#{expense.cost.to_formatted_s(:delimited)}円\n"
     end
 
+    text << "=================\n"
     text << "合計 #{expenses.sum(:cost).to_formatted_s(:delimited)}円"
   end
-
+  
   def pay_expenses(id)
     return 'IDの形式が不正です。半角数字で入力してください。' if id.blank?
-
+    
     ids = id.chomp.split(",").map do |n|
       return 'IDの形式が不正です。半角数字で入力してください。' if n.to_i == 0
       n.to_i
     end
-
+    
     expenses = Expense.where(id: ids, paid: false)
-
+    
     return '指定されたIDは、既に精算が済んでいます。' if expenses.blank?
-
+    
     text = "以下の費用を精算しました。\n"
     expenses.each do |expense|
-      text << "#{expense.id} #{expense.name} #{expense.cost.to_formatted_s(:delimited)}円\n"
+      text << "#{expense.id}|#{expense.name}|#{expense.cost.to_formatted_s(:delimited)}円\n"
     end
+
+    text << "=================\n"
     text << "合計 #{expenses.sum(:cost).to_formatted_s(:delimited)}円"
 
     expenses.update_all(paid: true)
